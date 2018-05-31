b0b62e3c9cdf1793b553390262c0d753f862aa67
Jonathan McIlnay-Moe <jmacmoe@gmail.com>
2018-05-31 08:42:48 -0700

    Just repeats what the user types in at the prompt.

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::io::{self, Write};
+
+fn main() {
+    println!("Lispr Version 0.1.0");
+    println!("Press Ctrl+C to Exit");
+
+    loop {
+        print!("lispr> ");
+        io::stdout().flush()
+            .expect("Failed to output prompt");
+
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input)
+            .expect("Failed to read line");
+
+        println!("{}", input);
+    }
+}
